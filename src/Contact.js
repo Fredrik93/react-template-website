34cf9c888524ad5ac66034f30eff6b181a39a13d
Fredrik93 <fredrikullman93@gmail.com>
2020-07-26 20:09:43 +0200

    inital pages'

---
+++ src/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Contact = () => (
+    <div>
+        <h2>Contact Us </h2>
+        <p> just some text about us </p>
+    </div>
+)
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => (
+    <div>
+        <h2>Headline yo ! </h2>
+        <p>Bacon ipsum dolor amet strip steak capicola hamburger andouille, pig pork bresaola meatball porchetta burgdoggen. Ham hock meatloaf pastrami chicken pig, chislic kevin jowl landjaeger hamburger. Chislic sirloin frankfurter, short ribs capicola short loin ham hock meatball sausage jowl prosciutto shankle pancetta chicken. Turducken pastrami pork cow, shankle cupim tail chuck frankfurter bresaola shoulder leberkas pork loin.
+            </p>
+    </div>
+)
