dfb9bb78efaaf6b88883c4c6fadb807e1c4d2c5c
Fredrik93 <fredrikullman93@gmail.com>
2020-07-27 09:38:55 +0200

    update appJS

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import { About } from './About'
 import { Contact } from './Contact'
 import { NoMatch } from './NoMatch'
 import { Layout } from './components/Layout'
+import { NavigationBar } from './components/NavigationBar'
+import { Jumbotron } from './components/Jumbotron'
 function App() {
     return (
         //this creates a wrapper, so we dont need a div to wrap all my components.  
         <React.Fragment>
+            <NavigationBar />
+            <Jumbotron />
             <Layout>
                 <Router>
                     <Switch>
