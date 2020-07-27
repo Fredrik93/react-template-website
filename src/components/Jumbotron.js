ccec22a22f39f1749946ddd66cf8f611172c0b12
Fredrik93 <fredrikullman93@gmail.com>
2020-07-27 09:30:08 +0200

    add jumbotron

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Jumbotron as Jumbo, Container } from 'react-bootstrap'
+import styled from 'styled-components'
+import boatImage from '../assets/boat.jpg';
+
+const Styles = styled.div`
+    .jumbotron {
+        background: url(${boatImage}) no-repeat fixed bottom;
+        background-size: cover;
+        color: #ccc;
+        height: 200px;
+        position: relative;
+ //says if it should be on top or bottom compared to other divs
+        z-index: -2;
+    }
+    .overlay {
+        background-color: #000;
+        opacity: 0.6;
+        position:absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        z-index: -1;
+    }
+`;
+export const Jumbotron = () => (
+    <Styles>
+        <Jumbo fluid className="jumbo">
+            <div className="overlay"></div>
+            <Container>
+                <h1>Welcome</h1>
+                <p>Learn to code from my youtube videos.</p>
+            </Container>
+        </Jumbo>
+    </Styles>
+)
