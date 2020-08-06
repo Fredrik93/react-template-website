2554f489b9c60172b3cdfc2c3cd3bbf41725bc65
Fredrik Ullman <fredrikullman93@gmail.com>
2020-08-06 14:52:35 +0200

    update jumbo

--- src/About.js
+++ src/About.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 export const About = () => (
     <div>
-        <h2>Headline yo ! </h2>
-        <p>Bacon ipsum dolor amet strip steak capicola hamburger andouille, pig pork bresaola meatball porchetta burgdoggen.</p> <p> Ham hock meatloaf pastrami chicken pig, chislic kevin jowl landjaeger hamburger. Chislic sirloin frankfurter, short ribs capicola short loin ham hock meatball sausage jowl prosciutto shankle pancetta chicken. Turducken pastrami pork cow, shankle cupim tail chuck frankfurter bresaola shoulder leberkas pork loin.
-            </p>
+        <h2>About Us  </h2>
+        <p>Welcome to the about page! </p> 
     </div>
 )
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -30,7 +30,7 @@ export const Jumbotron = () => (
             <div className="overlay"></div>
             <Container>
                 <h1>Welcome</h1>
-                <p>Learn to code from my youtube videos.</p>
+                <p>A jumbotron ! </p>
             </Container>
         </Jumbo>
     </Styles>
